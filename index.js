7f8dc6b6c73e52759a500a62a4a9b78a365cb455
Landon <landoncramer1@gmail.com>
2023-09-14 10:41:14 -0700

    domcontentloaded finish

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+const text = document.getElementById('text');
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  text.textContent = "This is really cool!";
+});
