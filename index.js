54e0377fa34270c01c9540a74f7762f25ea93a4b
Landon <landoncramer1@gmail.com>
2023-09-16 20:27:48 -0700

    dom content loaded

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 let text = document.querySelector('#text');
 document.addEventListener('DOMContentLoaded', () =>{
   console.log('text loaded');
-  text.textContent = 'This is awesome';
+  text.textContent = 'This is really cool!';
 })
